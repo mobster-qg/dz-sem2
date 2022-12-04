77a4c9dbea105ba129ccab9610e77fc32006189c
Achelov efim <mobster-qg@@mail.ru>
2022-12-05 05:21:26 +0700

    z10

---
+++ z10/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+ Console.Clear();
+ Console.WriteLine($"Ввести число ");
+int num = int.Parse(Console.ReadLine());
+
+int result = (  (num % 100) & (num /= 10) ); 
+
+if (num > 0) num = num;
+
+while (num > 100) num /= 10;
+if (num > 10)  num= num;
+while (num > 10) num %=10;
+
+
+Console.WriteLine($"{num} -> {num%10} Второе число");
