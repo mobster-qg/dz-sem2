238bb99e3e6f52a92cf04102bee41946ff7fbed2
Achelov efim <mobster-qg@@mail.ru>
2022-12-06 03:51:11 +0700

    dz15

---
+++ z15/Program.cs
@@ -0,0 +1,68 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.Clear();
+Console.WriteLine($"Ввести день от 1 до 7");
+int num = int.Parse(Console.ReadLine());
+ 
+ if (1 >= num )
+{
+        Console.WriteLine($"вых");
+}
+while( 2 > num );
+{
+    Console.WriteLine($"раб");
+}
+ return;
+  
+  if (3 >= num )
+{
+        Console.WriteLine($"вых");
+}
+while( 0 < num );
+{
+    Console.WriteLine($"раб");
+}
+ return;
+ 
+  if (4 >= num )
+{
+        Console.WriteLine($"вых");
+}
+while( 0 < num );
+{
+    Console.WriteLine($"раб");
+}
+ return; 
+ 
+ if ( 5 >= num )
+{
+        Console.WriteLine($"вых");
+}
+while( 0 < num );
+{
+    Console.WriteLine($"раб");
+}
+ return; 
+ 
+if (6 <= num )
+{
+        Console.WriteLine($"вых");
+}
+while ( 0 < num) 
+{
+    Console.WriteLine($"раб");
+}
+ return;
+ if (7 >= num )
+{
+        Console.WriteLine($"вых");
+}
+while( 0 < num );
+{
+    Console.WriteLine($"раб");
+}
+ return;
