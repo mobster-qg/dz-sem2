5a85d17d3718aebb79c5c537b70dd2a79bed3a31
Achelov efim <mobster-qg@@mail.ru>
2022-12-05 21:38:58 +0700

    dz13

---
+++ z13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine($"Ввести число ");
+int num = int.Parse(Console.ReadLine());
+
+int result = (num % 100); 
+
+if (num > 0)num = num;
+{
+    Console.WriteLine($"{num} -> {num%10} Третье число");
+}
+
+while (num <100) 
+{
+    Console.WriteLine($"{num} -> {num <0}Числа нет");
+  
+  return;
+}
